f0b02a8cf3f214b66258aa132b3c08b6133df853
Lauris <lauris.locmelis@awave.com>
2022-09-05 10:52:05 +0300

    Start

---
+++ Components/HouseCleaning.js
@@ -0,0 +1,12 @@
+import {Button, FlatList, Text, TextInput, ToastAndroid, View, PanResponder} from "react-native";
+import {styles} from "../css/styles";
+import React, {useEffect, useState} from "react";
+
+export const Home = () => {
+
+
+    return     <View style={styles.body} >
+      <p>This is start</p>
+
+    </View>
+}
