29e6d0c858b48bc8658bad6b7e1d216cc1b6c696
Zyflow <zyflow@gmail.com>
2022-10-24 13:36:07 +0300

    Branch updates

--- Components/Home.js
+++ Components/Home.js
@@ -1,19 +1,22 @@
-import {Text, View} from "react-native";
-import {styles} from "../css/styles";
+import { Text, View } from "react-native";
+import { styles } from "../css/styles";
 import React from "react";
-import {ServiceList} from "./ServiceList";
+import { ServiceList } from "./ServiceList";
 
-export const Home = ({navigation}) => {
+export const Home = ({ navigation }) => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          Kāds pakalpojums tev ir nepieciešams!
+        </Text>
+        <Text>
+          Izvēlieties vēlamo pakalpojumu, un mēs jums piemeklēsim atbilstošu
+          pakalpojumu sniedzēju
+        </Text>
 
-
-    return     <View style={styles.body} >
-        <View style={styles.container}>
-            <Text style={styles.headerText}>Kāds pakalpojums tev ir nepieciešams</Text>
-            <Text>Izvēlieties vēlamo pakalpojumu, un mēs jums piemeklēsim atbilstošu pakalpojumu sniedzēju</Text>
-
-            <ServiceList navigation={navigation}/>
-
-        </View>
-
+        <ServiceList navigation={navigation} />
+      </View>
     </View>
-}
+  );
+};
